68c48a5fd8ef2814950da75e547df58e1db5702e
evinracher <eevinley@gmail.com>
2026-09-08 11:19:17 -0500

    feat: Linear and Binary Search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
 
+    while (lo < hi) {
+        const mi = Math.floor(lo + (hi - lo) / 2);
+        const currValue = haystack[mi];
+        
+        if (currValue == needle) {
+            return true;
+        }
+
+        if (currValue < needle) {
+            lo = mi + 1;
+        } else {
+            hi = mi;
+        }
+    }
+
+    return false;
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,41 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  const step = Math.floor(Math.sqrt(breaks.length));
+  let hi = Math.min(step, breaks.length - 1);
+  let lo = 0;
 
+  do  {
+    if(breaks[hi]) {
+      for(let i = lo; i <= hi; i++) {
+        if(breaks[i]) {
+          return i
+        }
+      }
+    } else {
+      lo = hi + 1
+      hi = Math.min(hi + step, breaks.length - 1);
+    }
+  } while(lo < breaks.length)
+
+  return -1;
+}
+
+function course_two_crystal_balls(breaks: boolean[]): number {
+  const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+  let i = jumpAmount;
+
+  for (; i < breaks.length; i += jumpAmount) {
+    if (breaks[i]) {
+      break;
+    }
+  }
+
+  i -= jumpAmount;
+
+  for (let j = 0; j <= jumpAmount && i < breaks.length; ++j, ++i) {
+    if (breaks[i]) {
+      return i;
+    }
+  }
+
+  return -1;
 }
